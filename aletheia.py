541f0856fca9d80ed7f328fdfb681ade522add3f
Larry <laliucs1996@gmail.com>
2019-08-27 11:35:50 -0400

    Basic bot complete, intents need to be updated

--- aletheia.py
+++ aletheia.py
@@ -1,67 +1,126 @@
-# Larry Liu
-# Aletheia is a chatbot that focuses on greek mythology for discussion
-
-import numpy
-import tflearn
-import random
-import json
 import nltk
-import os
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-import tensorflow as tf
-
-if type(tf.contrib) != type(tf):
-    tf.contrib._warning = None
 
 from nltk.stem.lancaster import LancasterStemmer
 
 stemmer = LancasterStemmer()
 
+import numpy
+import tflearn
+
+import random
+import json
+import pickle
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow
 
 with open("intents.json") as file:
     data = json.load(file)
 
-word = []
-classes = []
-patA = []
-patB = []
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        word.append(wrds)
-        patA.append(pattern)
-        patB.append(intent["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-    if intent["tag"] not in classes:
-        classes.append(intent("tags"))
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-word = [stemmer.stem(w.lower()) for w in word]
-word = sorted(list(set(newClass)))
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-classes = sorted(classes)
+    labels = sorted(labels)
 
-training = []
-result = []
-zero = [0 for _ in range(len(classes))]
+    training = []
+    output = []
 
-for x, pat in enumerate(patA):
-    bag = []
-    letter = [stemmer.stem(w) for w in pat]
+    out_empty = [0 for _ in range(len(labels))]
 
-    for w in word:
-        if w in letter:
-            bag.append(1)
-        else:
-            bag.append(0)
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-        result_row = zero[:]
-        result_row[classes.index(patB[x])] = 1
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
         training.append(bag)
-        result.append(result_row)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("./model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
 
-training = numpy.array(training)
-output = numpy.array(result)
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        if results[0][results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg["responses"]
+
+            print(random.choice(responses))
+        else:
+            print("Truth is only given for what I know, please try again mortal.")
+
+
+chat()
+
