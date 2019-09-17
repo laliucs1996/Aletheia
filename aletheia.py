81f28f588118a57ac24ca8ad99690ceb1de22271
Larry <laliucs1996@gmail.com>
2019-09-17 15:25:31 -0400

    Optimization

--- aletheia.py
+++ aletheia.py
@@ -81,8 +81,11 @@ net = tflearn.regression(net)
 model = tflearn.DNN(net)
 
 
-model.fit(training, result, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
 
 def bag_of_words(s, word):
